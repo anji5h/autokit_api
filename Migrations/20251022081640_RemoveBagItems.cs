02283754d1f1d96d8b84a6f03c22f8cc77b7ee39
anjish <40964308+anji5h@users.noreply.github.com>
2025-10-23 11:27:55 +0200

    add migrations to source

---
+++ Migrations/20251022081640_RemoveBagItems.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace AutoKitApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBagItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BagItems");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BagItems",
+                columns: table => new
+                {
+                    BagItemId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProductId = table.Column<int>(type: "integer", nullable: false),
+                    Compartment = table.Column<int>(type: "integer", nullable: false),
+                    Quantity = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BagItems", x => x.BagItemId);
+                    table.ForeignKey(
+                        name: "FK_BagItems_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "ProductId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BagItems_ProductId",
+                table: "BagItems",
+                column: "ProductId");
+        }
+    }
+}
---
+++ Migrations/20251022094955_UpdatedBagId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AutoKitApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedBagId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BoxId",
+                table: "Bags",
+                newName: "BagId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BagId",
+                table: "Bags",
+                newName: "BoxId");
+        }
+    }
+}
