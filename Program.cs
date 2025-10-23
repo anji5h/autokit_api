2030b76954fdda7c2f81b3d522a4d5ef752dbcbc
anjish <40964308+anji5h@users.noreply.github.com>
2025-10-23 11:19:08 +0200

    added auto db migrate

--- Program.cs
+++ Program.cs
@@ -30,6 +30,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1"); });
 }
 
+if (app.Environment.IsProduction())
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
