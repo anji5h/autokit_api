240b673c95a65ecc256c6880dfa64267a93e22a8
anjish <40964308+anji5h@users.noreply.github.com>
2025-10-23 13:28:41 +0200

    updated qrcode generator code

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,10 +1,8 @@
-﻿using System.Drawing;
-using System.Drawing.Imaging;
-using AutoKitApi.DTOs;
-using AutoKitApi.Models;
+﻿using AutoKitApi.DTOs;
 using AutoKitApi.Services;
 using Microsoft.AspNetCore.Mvc;
 using QRCoder;
+using Color = System.Drawing.Color;
 
 namespace AutoKitApi.Controllers;
 
@@ -47,18 +45,20 @@ public class ProductController : ControllerBase
     public IActionResult GenerateQrCode([FromBody] QrCreateDto qrCreateDto)
     {
         var baseUrl = $"{Request.Scheme}://{Request.Host}";
-        var url = $"{baseUrl}/api/operation?bagId={qrCreateDto.BagId}&productId={qrCreateDto.ProductId}&quantity={qrCreateDto.Quantity}";
-
+        var payload =
+            $"{baseUrl}/api/operation?bagId={qrCreateDto.BagId}&productId={qrCreateDto.ProductId}&quantity={qrCreateDto.Quantity}";
+        
         using var qrGenerator = new QRCodeGenerator();
-        using var qrCodeData = qrGenerator.CreateQrCode(url, QRCodeGenerator.ECCLevel.Q);
-        using var qrCode = new QRCode(qrCodeData);
-        using var qrBitmap = qrCode.GetGraphic(5);
+        QRCodeData qrCodeData = qrGenerator.CreateQrCode(payload, QRCodeGenerator.ECCLevel.Q);
 
-        using var stream = new MemoryStream();
-        qrBitmap.Save(stream, ImageFormat.Png);
-        var imageBytes = stream.ToArray();
+        using var qrCode = new PngByteQRCode(qrCodeData);
+        byte[] qrCodeBytes = qrCode.GetGraphic(
+            pixelsPerModule: 10,
+            darkColor: Color.Black,
+            lightColor: Color.White,
+            drawQuietZones: true);
 
         var qrCodeName = $"qr_code_{DateTime.Now.Ticks}.png";
-        return File(imageBytes, "image/png", qrCodeName);
+        return File(qrCodeBytes, "image/png", qrCodeName);
     }
 }
